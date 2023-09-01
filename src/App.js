76116307c36d228f17094dda1a89d4e5675f404c
오창욱 <o2556@naver.com>
2023-09-01 09:30:18 +0900

    저장

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   const [userName, setUserName] = useState("");
   const [quizList, setQuizList] = useState(QuizList)
   const quiz = [...QuizList]
+  const [selected, setSelected] = useState(3);
 
   const ChangeEvent = (data) =>{
     const classValue = data.target.className;
@@ -19,28 +20,33 @@ function App() {
     switch(true){
       // includes("값") > 해당 문자열이 있는지 체크하는 속성
       case classValue.includes("name") :
-        console.log("이름값이 바뀜")  
+        setUserName(dataValue)  
       break;
       case classValue.includes("random") :
-        console.log("바뀜")
+        (dataValue === "1" ? setQuizList([...QuizList].sort(()=>{
+          return Math.random() - 0.5
+        })) : setQuizList([...QuizList]))
+        // 0 ~ 1사이의 값을 반환 > 0.5의 평균값을 얻게 되어 -0.5 ~ 0.5값으로 랜덤 출력
       break;
       case classValue.includes("cnt") :
         console.log("값 바뀜")
       break;
       case classValue.includes("type") :
-        console.log("타입 바뀜")
+        (dataValue === "전체" ? setQuizList([...QuizList]) : setQuizList([...QuizList].filter((e)=>{
+          return e.type === dataValue
+        })))
       break;
       default:
         console.log("데이터 없음")
     }
   }
   useEffect(()=>{
-
-  },[]) //로딩후 딱 한번만 실행되는 문법 ,[] - 바뀌는 것 만 []안에 넣으면 된다.
+    console.log(quizList)
+  },[quizList]) //로딩후 딱 한번만 실행되는 문법 ,[] - 바뀌는 것 만 []안에 넣으면 된다.
   return (
     <>
       <Routes>
-        <Route path='/' element={<Main ChangeEvent={ChangeEvent}  userName={userName}  quizList={quizList}/>}/>
+        <Route path='/' element={<Main ChangeEvent={ChangeEvent}  userName={userName}  quizList={quizList} quiz={quiz} selected={selected} />}/>
         <Route path='/quiz' element={<Detail quizList={quizList} userName={userName} />}/>
       </Routes>
     </>
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { useNavigate } from 'react-router-dom'
 
-function Main({userName, ChangeEvent, quizList}) {
+function Main({userName, ChangeEvent, quizList, quiz, selected}) {
   let navigate = useNavigate();
 
-
+  const type = quiz.map(item => item.type)
+  const typeSelect = [...new Set(type)] // 중복을 제거하는 코드
+  console.log(typeSelect)
   
 
   const nameChk = () =>{
@@ -24,7 +26,7 @@ function Main({userName, ChangeEvent, quizList}) {
             <div className='text-center flex flex-wrap justify-between'>
               <input defaultValue={userName} onChange={ChangeEvent}  type='text' placeholder='이름을 입력해주세요.' className='name border pl-4 py-2  rounded-md shadow-md outline-none basis-full sm:basis-8/12 ' />
               <button className='text-sm sm:text-base bg-blue-500 btn-primary hover:bg-blue-700 focus:ring-blue-400 sm:py-0 basis-full sm:basis-3/12 mt-5 sm:mt-0' onClick={nameChk}>시작하기</button>
-              <h3 className="my-5 basis-full text-center">{userName !== "" && <> <span className='text-indigo-500 font-bold'>{userName}님</span> <span>문제 유형은 기본값으로 설정 되어있으며, 총 문제 중 1문제를 선택하셨습니다.</span></>}</h3>
+              <h3 className="my-5 basis-full text-center">{userName !== "" && <> <span className='text-indigo-500 font-bold'>{userName}님</span> <span>문제 유형은 기본값으로 설정 되어있으며, 총 {quiz.length}문제 중 1문제를 선택하셨습니다.</span></>}</h3>
             </div>
           </div>
           <div className="w-full bg-white rounded-lg p-5 mt-5 flex justify-between flex-wrap items-center">
@@ -37,16 +39,23 @@ function Main({userName, ChangeEvent, quizList}) {
             </div>
             <div className="flex justify-around items-center xl:basis-4/12 basis-full">
               <button className='btn-primary text-sm sm:text-base bg-green-800 hover:bg-green-700 focus:ring-green-400 basis-5/12 my-5'>갯수설정</button>
-              <select onChange={ChangeEvent} className='cnt border rounded basis-6/12 text-center py-1.5'>
-                <option value="0">1문제</option>
-                <option value="1">2문제</option>
+              <select onChange={ChangeEvent} defaultValue={selected} className='cnt border rounded basis-6/12 text-center py-1.5'>
+                {
+                  Array(quizList.length).fill().map((e,i)=>{
+                    return <option value={i+1} key={i}>{i+1} 문제</option>
+                  })
+                }
               </select>
             </div>
             <div className="flex justify-around items-center xl:basis-4/12 basis-full">
               <button className='btn-primary text-sm sm:text-base bg-green-800 hover:bg-green-700 focus:ring-green-400 basis-5/12'>문제유형</button>
               <select onChange={ChangeEvent} className='type border rounded basis-6/12 text-center py-1'>
-                <option value="0">전체 (문제)</option>
-                <option value="1">옵션 (0문제)</option>
+                <option value="전체">전체 ({quiz.length}문제)</option>
+                {
+                  typeSelect.map(el=>{
+                    return <option value={el} key={el}>{el} ({quiz.filter(e=> el === e.type).length}문제)</option> // => 후 {}를 하면 return문이 필요하고 {}가 없으면 return 생략가능
+                  })
+                }
               </select>
             </div>
           </div>
